b3f895bd4cf825ec20beb56ee8f15516ad34492d
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2020-02-24 14:06:43 -0300

    update

---
+++ GameBetyDay/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
