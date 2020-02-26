5ccc19def95bf210fa73b3fdb7d59183879d16c9
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2020-02-26 05:13:40 -0300

    First Scene Development

---
+++ GameBetyDay/Assets/Scenes/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+
+   void Update(){
+      if(Input.GetMouseButtonDown(0))
+      {
+         GoNextScene();
+      }
+
+   }
+   public void GoNextScene(){
+	   
+	   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+   }
+    
+}
